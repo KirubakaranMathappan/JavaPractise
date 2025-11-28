b2b5f5c64949d3ccc5aa556595045d8f4b4775aa
KirubakaranMathappan <m.kirubakaranoct13@gmail.com>
2025-11-28 10:05:22 +0530

    JavaPractise01

---
+++ src/org/test/Practise.java
@@ -0,0 +1,34 @@
+package org.test;
+
+public class Practise {
+	public static void main(String[] args) {
+		
+		
+	String a="2";
+	String b="4";
+	
+	int c=Integer.parseInt(a);
+	int d=Integer.parseInt(b);
+	
+	System.out.println(c+d);
+	
+	int a1[] = {3,6,9};
+	 
+		System.out.println(a1[1]*2);
+		System.out.println(6 % 4);
+		System.out.println(9 / 4);
+	
+		int x=9;
+		if (x % 2 == 0) {
+			System.out.println("Even");			
+		}
+		else {
+			System.out.println("odd");
+		}
+	
+		
+		
+	}
+
+	
+}
